5f775d9da9a8d771c101b529a63381969e8f9245
Nathan Gonzalez <ncgonz@gmail.com>
2020-08-03 20:50:06 -0500

    Add project files.

---
+++ ClassesExample/Animals/Chicken.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExample.Animals
+{
+    class Chicken
+    {
+        //property
+        public string Name { get; set; }
+
+        //field
+        private bool _isHungry = true;
+
+        //default constructor
+        //public Chicken() { }
+
+        //constructor
+        public Chicken(string name)
+        {
+            Name = name;
+            Console.WriteLine($"{name} got instantiated...");
+        }
+
+        //method (signature = accessibility modifier -> return type -> Name -> Parameters
+        public void Peck(string typeOfFood)
+        {
+            if (_isHungry)
+            {
+                Console.WriteLine($"{Name} pecks hungrily at the {typeOfFood}.");
+                _isHungry = false;
+            }
+            else
+            {
+                Console.WriteLine($"{Name} is not hungry.");
+            }
+        }
+    }
+}
---
+++ ClassesExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using ClassesExample.Animals;
+using System;
+
+namespace ClassesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chicken = new Chicken("Liza");
+            var bob = new Chicken("Bob");
+            
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+
+
+            bob.Peck("watermelon");
+
+            Console.WriteLine($"{chicken.Name} and {bob.Name} are chickens......" );
+
+
+        }
+    }
+}
